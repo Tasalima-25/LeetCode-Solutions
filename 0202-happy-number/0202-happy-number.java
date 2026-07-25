c60e808e5edb5f0195fe0473b51f630a643b4c69
Tasalima Jamadar <143584159+Tasalima-25@users.noreply.github.com>
2026-07-25 23:01:26 +0530

    Time: 0 ms (100%), Space: 42 MB (95.34%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public boolean isHappy(int n) {
+
+        int slow = n;
+        int fast = getSum(n);
+
+        while (fast != 1 && slow != fast) {
+            slow = getSum(slow);
+            fast = getSum(getSum(fast));
+        }
+
+        return fast == 1;
+    }
+
+    private int getSum(int n) {
+
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+
+        return sum;
+    }
+}
