56c07e42558589c71a7a586ba155f49500bb6881
Tasalima Jamadar <143584159+Tasalima-25@users.noreply.github.com>
2026-07-27 22:59:10 +0530

    Time: 3 ms (81.52%), Space: 43.6 MB (67.99%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -0,0 +1,45 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+
+        int m = num1.length();
+        int n = num2.length();
+
+        int[] result = new int[m + n];
+
+        // Multiply digits from right to left
+        for (int i = m - 1; i >= 0; i--) {
+
+            for (int j = n - 1; j >= 0; j--) {
+
+                int digit1 = num1.charAt(i) - '0';
+                int digit2 = num2.charAt(j) - '0';
+
+                int product = digit1 * digit2;
+
+                int position1 = i + j;
+                int position2 = i + j + 1;
+
+                int sum = product + result[position2];
+
+                result[position2] = sum % 10;
+                result[position1] += sum / 10;
+            }
+        }
+
+        StringBuilder answer = new StringBuilder();
+
+        for (int num : result) {
+
+            // Skip leading zeros
+            if (!(answer.length() == 0 && num == 0)) {
+                answer.append(num);
+            }
+        }
+
+        return answer.toString();
+    }
+}
